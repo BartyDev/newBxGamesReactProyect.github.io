cde2afcbf59d86c070052c4b55649e5f5e7e0f11
barty91 <elvis.velasquez91@gmail.com>
2022-11-27 21:41:18 -0300

    agregando navbar

--- bx-games-virtual-shop/src/components/NavBar.jsx
+++ bx-games-virtual-shop/src/components/NavBar.jsx
@@ -1,43 +1,63 @@
 import React from "react";
+import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
         <div className="row">
-            <div className="col-md-6">
-                <nav className="navbar navbar-expand-lg bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/HOME"><img src={"imgs/bx_game_logo.png"} alt={"Logo De Bx Game Store"} width={100}/></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+            <nav className="navbar navbar-expand-lg" aria-label="Offcanvas navbar large">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/HOME"><img src={"imgs/bx-game-logo-bg0.png"} alt={"Logo De Bx Game Store"} width={120} /></a>
+                    <button className="navbar-toggler custom-toggler fs-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-start footerBackApp" tabIndex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+                        <div className="offcanvas-header">
+                            <a className="navbar-brand" href="/HOME"><img src={"imgs/bx-game-logo-bg0.png"} alt={"Logo De Bx Game Store"} width={100} /></a>
+                            <button type="button" className="btn-close btn-close-icon fs-2" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body navTextWhiteApp">
+                            <ul className="navbar-nav justify-content-center flex-grow-1 gap-3 colorRender">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/TENDENCIAS">TENDENCIAS</a>
+                                    <a className="nav-link colorActive" aria-current="page" href="/TENDENCIAS">TENDENCIAS</a>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="/TIENDA" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        TIENDA
+                                    </a>
+                                    <ul className="dropdown-menu sectionBackApp colorRender">
+                                        <li><a className="dropdown-item" href="/">PROXIMOS LANZAMIENTOS</a></li>
+                                        <li><a className="dropdown-item" href="/">RESERVAS</a></li>
+                                        <li>
+                                            <hr className="dropdown-divider colorActive" />
+                                        </li>
+                                        <li><a className="dropdown-item" href="/">CONSOLAS</a></li>
+                                        <li><a className="dropdown-item" href="/">RECURSOS</a></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="/SOPORTE-24-7" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    SOPORTE 24/7
+                                    </a>
+                                    <ul className="dropdown-menu sectionBackApp colorRender">
+                                        <li><a className="dropdown-item" href="/">CONTACTOS</a></li>
+                                        <li>
+                                            <hr className="dropdown-divider colorActive" />
+                                        </li>
+                                        <li><a className="dropdown-item" href="/">PREGUNTAS FRECUENTES</a></li>
+                                    </ul>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/TIENDA">TIENDA</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/CONTACTO">CONTACTO</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/RECURSOS">RECURSOS</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/SOPORTE-24-7">SOPORTE 24/7</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" href="/error-404">error 404</a>
+                                    <a className="nav-link disabled text-success" href="/error-404">error 404</a>
                                 </li>
                             </ul>
+                            <hr className="colorActive" />
+                            <div className="col-md-1">
+                                <CartWidget />
+                            </div>
                         </div>
                     </div>
-                </nav>
-            </div>
-            <div className="col-md-6">
-                <p>CartWidget</p>
-            </div>
+                </div>
+            </nav>
         </div>
     )
 }
